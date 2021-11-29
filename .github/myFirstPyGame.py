14f98b85d51738bc3287f20f2f6f43d9561228b6
Beau DP <s12469680@students.duvalschools.org>
2021-11-29 14:28:30 -0500

    v0.1
    imported pygame code, setup colors

--- .github/myFirstPyGame.py
+++ .github/myFirstPyGame.py
@@ -1,1 +1,18 @@
-# My First PyGame, Beau Diaz-Perez, 11/29/2021 1:59pm v0.0
+# My First PyGame, Beau Diaz-Perez, 11/29/2021 1:59pm v0.3
+
+import pygame, sys
+from pygame.locals import *
+
+#start pygame
+pygame.init()
+
+#setup our window.
+windowSurface = pygame.display.set_mode((500,400), 0, 32)
+pygame.display.set_caption('hello, world!')
+
+#setup colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
